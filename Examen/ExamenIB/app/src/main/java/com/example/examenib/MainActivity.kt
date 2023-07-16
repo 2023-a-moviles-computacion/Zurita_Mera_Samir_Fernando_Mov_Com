12c694d3077c67f8715a0cd183da2f42adc9c40b
SamirZm2304 <samir.zurita@gmail.com>
2023-07-16 00:24:03 -0500

    Examen IB

--- Examen/ExamenIB/app/src/main/java/com/example/examenib/MainActivity.kt
+++ Examen/ExamenIB/app/src/main/java/com/example/examenib/MainActivity.kt
@@ -1,22 +1,26 @@
+package com.example.examenib
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import com.example.examenib.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnZapatos = findViewById<Button>(R.id.btnZapatos)
+        val btnMarcas = findViewById<Button>(R.id.btnMarcas)
+
         btnZapatos.setOnClickListener {
-            // Realizar acciones al hacer clic en el botón de Zapatos
+            val intent = Intent(this, ZapatosActivity::class.java)
+            startActivity(intent)
         }
 
-        val btnMarcas = findViewById<Button>(R.id.btnMarcas)
         btnMarcas.setOnClickListener {
-            // Realizar acciones al hacer clic en el botón de Marcas
+            val intent = Intent(this, MarcasActivity::class.java)
+            startActivity(intent)
         }
     }
 }
